07c73fae97db56f772d5c3eb881948708c89eb43
jamesyu <zetaentranceserious@gmail.com>
2016-02-11 10:43:20 +0800

    Solve dashboard and public layout conflict

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,6 +1,12 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require bootstrap
+//= require jquery.min
+//= require d3
+//= require assay
+//= require d3pie.min
+//= require liquid
 //= require Chart.min
 //= require bootstrap-switch.min
 //= require jquery.matchHeight-min
